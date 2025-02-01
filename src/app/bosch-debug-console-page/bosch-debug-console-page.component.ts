0a1cab0cdea47bdf4e394bf819ae217e8ff91cff
EnselerizeR <enselerizer@gmail.com>
2025-02-02 00:08:09 +0300

    Add Bosch connected devices scan UI

--- src/app/bosch-debug-console-page/bosch-debug-console-page.component.ts
+++ src/app/bosch-debug-console-page/bosch-debug-console-page.component.ts
@@ -3,18 +3,32 @@ import { FormsModule } from "@angular/forms";
 import { ConnectionStatus, DataProviderBoschService } from '../data-provider-bosch.service';
 import { IpcService } from '../ipc.service';
 import { Subscription } from 'rxjs';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { bootstrapArrowClockwise } from '@ng-icons/bootstrap-icons';
+
+
+enum SystemLogMessage {
+  CONNECTED =         '--------- CONNECTED ---------',
+  CONNECTION_FAILED = '----- CONNECTION FAILED -----',
+  DISCONNECTED =      '------- DISCONNECTED --------',
+  SEND_ERROR =        '---- ERROR WHILE SENDING ----'
+}
+
 
 @Component({
   selector: 'app-bosch-debug-console-page',
-  imports: [FormsModule],
+  imports: [FormsModule, NgIcon],
+  providers: [provideIcons({ bootstrapArrowClockwise })],
   templateUrl: './bosch-debug-console-page.component.html',
   styleUrl: './bosch-debug-console-page.component.css'
 })
+
 export class BoschDebugConsolePageComponent implements OnInit, OnDestroy {
 
   readonly ConnectionStatus = ConnectionStatus;
 
   COMPort: number = 0;
+  COMPortList: number[] = [];
   logBuffer: string = '';
   logBufferArr: string [] = [];
   logAreaRows: number = 10;
@@ -37,10 +51,12 @@ export class BoschDebugConsolePageComponent implements OnInit, OnDestroy {
       this.cdr.detectChanges();
     }));
     this.subscriptions.push(this.dataProviderBocsh.cissPortsList.subscribe((portsList: number[]) => {
+      this.COMPortList = portsList;
       if (portsList.length) {
         this.setCOMPort(portsList.at(-1)!);
+      } else {
+        this.setCOMPort(0);
       }
-      this.cdr.detectChanges();
     }));
     this.subscriptions.push(this.dataProviderBocsh.incomingDataRaw.subscribe((rawData: string) => {
       if(rawData) {
@@ -67,6 +83,7 @@ export class BoschDebugConsolePageComponent implements OnInit, OnDestroy {
 
   setCOMPort(port: number) {
     this.COMPort = port;
+    this.cdr.detectChanges();
   }
   getCOMPort() {
     return this.COMPort;
@@ -86,6 +103,11 @@ export class BoschDebugConsolePageComponent implements OnInit, OnDestroy {
     return this.command;
   }
 
+  updateCissPortsList() {
+    this.dataProviderBocsh.updateCissPortsList();
+  }
+
+
   logLine(line: string) {
     this.logBufferArr.push(line);
     while(this.logBufferArr.length > this.logAreaRows) {
@@ -103,15 +125,15 @@ export class BoschDebugConsolePageComponent implements OnInit, OnDestroy {
 
   onClickConnectBtn() {
     this.dataProviderBocsh.connect(this.COMPort).then(() => {
-      this.logLine('--------- CONNECTED ---------');
+      this.logLine(SystemLogMessage.CONNECTED);
     }, () => {
-      this.logLine('----- CONNECTION FAILED -----');
+      this.logLine(SystemLogMessage.CONNECTION_FAILED);
     })
   }
 
   onClickDisconnectBtn() {
     this.dataProviderBocsh.disconnect().then(() => {
-      this.logLine('------- DISCONNECTED --------');
+      this.logLine(SystemLogMessage.DISCONNECTED);
     });
   }
 
@@ -120,7 +142,7 @@ export class BoschDebugConsolePageComponent implements OnInit, OnDestroy {
       this.dataProviderBocsh.debugSendCommand(this.command).then(() => {
         this.command = '';
       }, () => {
-        this.logLine('ERROR while sending');
+        this.logLine(SystemLogMessage.SEND_ERROR);
       });
     }
   }
